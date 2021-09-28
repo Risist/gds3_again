58fe92480b71549c151fea62ef09173a16a9a1ff
jzataj <jakub.zataj@gmail.com>
2021-09-28 19:07:08 +0200

    Placeholder sounds events for enemies
    - As enemies use the same animations as player (some at least), lack of sound events for them caused errors in console. They are fixed now.

--- Gds_3/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Gds_3/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,15 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID SOUNDPLAY = 4043471592U;
+        static const AkUniqueID PLAY_ENEMY_ATTACK = 2257393085U;
+        static const AkUniqueID PLAY_ENEMY_BODYFALL = 1222472514U;
+        static const AkUniqueID PLAY_ENEMY_DASH = 1673311249U;
+        static const AkUniqueID PLAY_ENEMY_FOOTSTEP = 3668695751U;
+        static const AkUniqueID PLAY_PLAYER_ATTACK = 3238800884U;
+        static const AkUniqueID PLAY_PLAYER_BODYFALL = 606831743U;
+        static const AkUniqueID PLAY_PLAYER_DASH = 2175711460U;
+        static const AkUniqueID PLAY_PLAYER_DEAD = 2074354138U;
+        static const AkUniqueID PLAY_PLAYER_FOOTSTEP = 1724675634U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -24,13 +32,25 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
+        static const AkUniqueID MAIN = 3161908922U;
     } // namespace BANKS
 
     namespace BUSSES
     {
+        static const AkUniqueID _3D_ENV = 2487412084U;
+        static const AkUniqueID CONST_ENV = 315211798U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID GUI = 915214414U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REV_ROOM_01 = 2681376660U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
